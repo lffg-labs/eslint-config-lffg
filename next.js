f85570de6c7e0538a055529367a5305af4f41cc1
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-03-24 22:44:23 -0300

    feat: Add Next.js config

---
+++ next.js
@@ -0,0 +1,16 @@
+const WARN = 'warn';
+const OFF = 'off';
+
+const pagesGlob = 'pages/*.{js,jsx,ts,tsx}';
+
+module.exports = {
+  overrides: [
+    {
+      files: [pagesGlob, `src/${pagesGlob}`],
+      rules: {
+        'import/prefer-default-export': WARN,
+        'import/no-default-export': OFF
+      }
+    }
+  ]
+};
